5c5b5a84d91f2257bf498a1b9814858a3ae465eb
isaacmuchiri <kevinisaacmugambi@gmail.com>
2024-08-22 03:26:48 +0300

    changes made

--- learn1.php
+++
@@ -1,5 +0,0 @@
-
-I am thrilled to present my proffessional photography services with the best quality 
-cameras.Committed to delivering exceptional results and creating cherished memories.
-I am thrilled to present my proffessional photography services with the best quality 
-cameras.Committed to delivering exceptional results and creating cherished memories.
